2a54972769d4f12b2ed0bc6f73fa667407eb0698
ianroydasmarinas <ianroy.dasmarinas@g.msuiit.edu.ph>
2026-09-17 21:31:02 +0800

    Add you look familiar activity

---
+++ activities/Your face is familiar activity/you look familiar.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <conio.h>
+void main(){
+printf("                                         ....:::::::...\n");
+printf("                                ....:::^^^^^^^^^^^^^^^^^^^^^^::.\n");
+printf("                            ...:^^~~^^^^^^^^^^^:::::::^^^~~~~~~~^::.\n");
+printf("                          .:^^~~~~^^^^:::^^^^^::::^:::::::^^^^^^^^^^^:..\n");
+printf("                        .^~~~~~~^^:::::::::::^::::^^^^^:::^^^^^^^^^^^^^^:.\n");
+printf("                      .:~~~~~~^^^:::::::::::::::::^::::::^:^^^^^^^::^^:^^^^:.\n");
+printf("                   .:^~~~^^~~~^^^::^^::::::^^^^^^^^^~^^^^^^^^^^^:::::^::::^~~^:.\n");
+printf("                 .^~~~~^^^~~~^^^^^^^::^^^:^^^^^~~~~!777!!77!~~~^^:::::::::::~~~^:.\n");
+printf("               .^~~~~^^^^~~~^^^^^^^:^^^^^^^^^~^!7!777????7777!~~^^^:::::...::^^^^^:.\n");
+printf("             .^~!!~~^^^~~~~^~~~~^^^^~~^^^:^^^^~!777?7??JJ77?77!~~^^::^^:...:::::^^^^:\n");
+printf("            .~!!!~~~^^^^^~^^~!~^^^~~~^^^^^^^^^~!7????JJJJ?7777!!!~^:::^:::.:::.:::::^:.\n");
+printf("           .!!!!~~~~~^^^^^^^~~^^^~~~^~!~^:^^~~~~!??7??JJ???7!7!~!!~^^^:^:...::..:::::^:..\n");
+printf("           ~!!!!~~~~^^^^^^:^^^^^^^^^~!!~~:^~!!~~!!77?JYJJJJ?777~~~~^^^:::...::....::::^:.\n");
+printf("          .!!~!~~^~^^^^::^^^^^::^^^!!!7!!^^~!!~~!!7?JJYYYYJJ?7!~^~^^^^::::...:......::^^..\n");
+printf("          .~!!!~^^^^:::::::::::^^~!7777?77^^~!~!!!7?JYY55YYYJJ!!~~^^^^^::....:......:::^:.\n");
+printf("           .~!!~^^^^::::::::::^^!77???JYJ7~^^^~!77??JY55555YYJ7!!^^^~~~::...::......:::^.\n");
+printf("           .:!!~^^^^^:::::::::^~!~!7?YYYJ?7!~~!!!7?JJYYY555YYJ7!~^^^~~~^::...... ..:::^:.\n");
+printf("             ^!!~^^^^::::::::::::::^~!7???JJ7~~!!!!?JJJYYJJ?7!~^^:.:::^^^:.....  ..::::.\n");
+printf("             .~!~~^^^:::::::^^^~~~~^^^~!???JJ?7~!77?JJ??7!~^^:::::::...::::.......::::\n");
+printf("              :~~~~^^^~^^!~^777????777!777?JJJJ???????7!!~~~~~~!!!~~^^^^^^~^::...::::\n");
+printf("               ^~~~^^!!77?77????????777?????JYYJJJ??777!!!!!!!777777!!!!!!!!~^:::::^.\n");
+printf("               .~~~~~JJJJYJ?77!!!~^^^~!77???Y555YJJ?777!!!~~~^^^^^~~!77777777!^:::::\n");
+printf("                ^7!!?55YYYJ?7!!77:::.~7!!??J5PGGP5J??7!!!~~!^....~~^^~!77777??~:::::\n");
+printf("                ^55?5GPPP55YJJ??J7!~!????Y55PPGGGPYJ?777!~!77~^^~!~~~!!777?????^:^!!.\n");
+printf("                :YY5GGGGGGGGGP5YYJJJJJY5PPPPPGGGGP5YJ???????7777777777????????J~~7!7^\n");
+printf("                .JY5GGGGBBBBBGGGPPPPPPPGGGGGGGGBGP5YJJJJJJJJJJJJJJJJJJJJJJJJ??J!!7!!^\n");
+printf("                 ?PPGGGGBBBBBBBGGGGGGGGGGGGGGGGBBP5YYJJJJJYYYYYY55555YYYYYJJ??J!7?!!:\n");
+printf("                 ^G5PGGGGGGGGGGGGGGGGGGGGPPGGGGBBPP5YJ???JJYYY5555555555YYJJ?JJ!!?7!\n");
+printf("                 .P55GGGGGGGGGGGGGGGGGPPPPGGGGGGGP55YYJJJ?JYYY55555555YYJJJ?JJ?~~!7^\n");
+printf("                  5GPGGGGGGGGGGGGGGGPPP55GGGPPPP55YYJJJJY?JYYYY5555555YYJJ??JJ7~~!7.\n");
+printf("                  ?BGGGGPPGGGGGGGGGPPPP55P5J?J5YYJJ?7!!?J??YYY5555555YYJJ???JJ7777!\n");
+printf("                  .5BGGGGGGGGGGGPPPPPPPPP5J777?JJ??7!!!!7!7?Y5555555YYJJJ??JJ?7??7.\n");
+printf("                   .?5PGGGGGGGGGPPPPPPPGGGPYYYYJ?777777777??J555555YYYJJJJJJJ???~.\n");
+printf("                      .JGGGGGPGGPPPPPPGGPPPPPPP5J????77777??JY55YYYYYJJJJJJJ7^^.\n");
+printf("                       :GGGGGGPPPPPPPPPPPPPPP555555YYJ????JJJYYYYYYYJJJJJJJJ.\n");
+printf("                        7GGGGGPPPPPPP55555YJJJJJYJ??????JJJ?JJJJJYYJJJJJJJJ^\n");
+printf("                         JGGGPPPPPP55YJ?7777???77!!!!!~~!!!77??JJJJJJJJJJJ~\n");
+printf("                          JGGPPPPPP55YYYYY5Y5YYJJ?????777!!!7??JJJJJJJJJJ~\n");
+printf("                           ?GPPPPP55555PPPP55YYYYYJJJ???JJJJJJJJJJJJJJJJ^\n");
+printf("                            7GPPP55555555555YJ??77777????JJJJJJ???JJJY?:\n");
+printf("                            YGPPPP555555555555YYYYYJJJJJYJJJJJJ???JJY?.\n");
+printf("                           7&GGPPPPPPPPPPPPPPPPPPP555555YYYYJJJ?JJJJJ7\n");
+printf("                          :#&GGPPPPPPPPPPPPPPP555555555YYYJJJJJJJJJ??J!\n");
+printf("                          5@@BPPP5555PP55555YYYYYJYYYJJJJJJJJJJJJ????YP!.\n");
+printf("                       .:J@@@@BPPP5YYYY55YYYYJJJJJJJJJJ???JJJ?????77YPGPY?!~^:.\n");
+printf("                  :~7YGB&@@@@&@&G55YYYYJJJJJJJJJJJJJJ???????????77J5PPPP555PP55YJ?!^:.\n");
+printf("            .^!J5G#&@@@@@@@@&&&&&#P5YYYJJJJJJJ?J??????77?????77!?YPPPPPPPJY5PPPPPPPP55J?~^.\n");
+printf("      .^!J5G#@@@@@&&@@@@@@@@&&&&&&##G5JJJJJJJJJJ??????????77!!?YPPPPPPPPPYJ5PPPPPPPPPPPPPP5Y?!^.\n");
+printf("  :~JP#&@@@@@@@@&&&@@@@@@@@@&&&&&&&&&&B5J??????????????77!!7J5PGPGGGPPPPP5J5PPPPPPPPPP5PPPPPPPP55Y?7\n");
+printf("PB&@@@&&&&&@@@@@@&@@@@@@&@@@&&&&&&&&&&&&#PJ?????????77777J5PGGGGGGGPPPPPPPYPPPPGPPPPPPPPPP555PPPGGBB\n");
+printf("@@@@@&&##&@@@@@@@&&&@@&&&&@@@@@&&&&&&&&&&&#G5J7777777?Y5PGGGGGGGGGGGPPPPPPYPPPPGPPPPPPPPPPP5PPPGBB##\n");
+printf("@@&&&&&#&&&@@&@@&&&&&&@@&&@@&&@@@@&&&&&&&&&##BPYJJJ5PGGGPPGGGGGGPPPPPPP5P5YPPPGGGPPPPPPPPPPPPGBB####\n");
+printf("@&&&&&&&&&&&&&&@@&@@@@@&&&@@@@@@@@@&&&&&&&&&&&#BGGGPPPGGPPGGGGGGPPPPPPPPP55PPGGPPPPPPPPPPPGGB#######\n");
+printf("@@&#&#&&@@&&&&@@@@@@@@&&&@@@@@@@@@@@@@@@@@&&@&#GPPGGGP5PGGGGGGGGGPPPPPPGPYPPGGGPPPPPPPPPPBB#####B###\n");
+printf("@&#B#&&@@@&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@&#GPPPPPGGGPPPGGGGGGGGPPPGPPP55PGGGGPPPPPPPPGB#######B###\n");
+printf("@&###&&@@@@&&&@&&@@@@&&&&&@@&@@@@&@@@@@@@&BGPGGGGGGGGGP5PPPPGGPGPPPPPPPP5PPGGGGGPPPPPGB#############\n");
+}
+
+
+
